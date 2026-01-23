1db7401dd8f8c36cb0a9a2a8405f04696aae5b31
Tobias <tobiascosta064@gmail.com>
2026-01-22 22:38:28 -0300

    feat: deletando unicidade em Imagem model

--- models.py
+++ models.py
@@ -232,7 +232,7 @@ class Imagem(db.Model):
     __tablename__ = 'imagens' 
 
     id = db.Column(db.Integer, primary_key=True)
-    img = db.Column(db.LargeBinary, unique=True, nullable=False)
+    img = db.Column(db.LargeBinary, nullable=False)
     name = db.Column(db.Text, nullable=False)
     mimetype = db.Column(db.Text, nullable=False)
     
